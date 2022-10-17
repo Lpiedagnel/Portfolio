795b77fd2daec730f966065752a0d544136f930e
Lucas Piedagnel <piedagnel.dev@gmail.com>
2022-10-17 14:13:19 +0200

    Setup project with react and express this time. Add SASS.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Bienvenue sur mon Portfolio !</h1>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
